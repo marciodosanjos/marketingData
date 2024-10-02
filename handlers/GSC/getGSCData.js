e4e124c84d6c66adc8586af18afd0505dcd5d53d
Marcio <marciodosanjos@ymail.com>
2024-10-02 16:23:35 -0300

    ajuste erro func get GSCData

--- handlers/GSC/getGSCData.js
+++ handlers/GSC/getGSCData.js
@@ -6,7 +6,7 @@ const stringTodate = require("../../utils/stringToDate");
 // env variables
 require("dotenv").config();
 
-//GoogleSpreadsheet Instance with ID of document
+// GoogleSpreadsheet Instance with ID of document
 const doc = new GoogleSpreadsheet(
   "1V3Zq6eWgLO6HR6j4QD9W23RYWefdgLto5IhzSYvKHKA"
 );
@@ -45,13 +45,13 @@ const getGSCData = async () => {
     // Scope to access API Google Search Console
     const SCOPES = ["https://www.googleapis.com/auth/webmasters.readonly"];
 
-    // instance of autentication OAuth2 with credentials
+    // instance of authentication OAuth2 with credentials
     const auth = new google.auth.GoogleAuth({
       credentials: creds,
       scopes: SCOPES,
     });
 
-    //client OAuth2
+    // client OAuth2
     const authClient = await auth.getClient();
     const accessToken = await authClient.getAccessToken();
 
@@ -59,36 +59,33 @@ const getGSCData = async () => {
       console.log("No access token available");
       return;
     }
-    //Google Webmasters API instance with authClient
+
+    // Google Webmasters API instance with authClient
     const webmasters = google.webmasters({ version: "v3", auth: authClient });
 
-    //Websites
+    // Websites
     const res = await webmasters.sites.list({});
 
     // are there websites?
     if (res.data.siteEntry && res.data.siteEntry.length > 0) {
       console.log("Websites found:");
-      res.data.siteEntry.forEach((site) => {
+      const sitePromises = res.data.siteEntry.map(async (site) => {
         console.log(`${site.siteUrl} (${site.permissionLevel})`);
         let siteUrl = site.siteUrl;
 
-        async function getData() {
+        // Move getData function inside the map function to keep it scoped
+        const getData = async () => {
           try {
-            //load google sheet
+            // Load google sheet
             await doc.useServiceAccountAuth(googleSheetCredentials);
-
             const loadInfo = await doc.loadInfo();
 
-            // Acessar uma aba específica (pelo índice 0, a primeira aba)
+            // Access a specific sheet (by index 0, the first sheet)
             const seoPageSheet = doc.sheetsByIndex[0];
 
             const items = await seoPageSheet.getRows();
             const rawDates = items.map((item) => item.date);
             const { finalStartDate, finalEndDate } = stringTodate(rawDates);
-            // console.log(
-            //   "Executing query on the data " + finalStartDate,
-            //   finalEndDate
-            // );
 
             const query = {
               siteUrl: siteUrl,
@@ -105,7 +102,6 @@ const getGSCData = async () => {
             console.log(query);
 
             const response = await webmasters.searchanalytics.query(query);
-
             let formatedResponse = response?.data?.rows?.map((item) => ({
               id:
                 item.clicks +
@@ -129,20 +125,18 @@ const getGSCData = async () => {
               console.log("formatedResponse is not an array");
               console.log(response.data);
               console.log(formatedResponse);
-              process.exit(0); // Sucesso
-
-              return;
+              process.exit(1);
             }
 
             if (formatedResponse.length === 0) {
               console.log("No new items to upload");
-              process.exit(0); // Sucesso
+              process.exit(0);
             }
 
             if (formatedResponse.length !== 0 && items.length === 0) {
               console.log("New items to upload and the sheet is empty");
               await seoPageSheet.addRows(formatedResponse);
-              process.exit(0); // Sucesso
+              process.exit(0);
             }
 
             if (formatedResponse.length !== 0 && items.length !== 0) {
@@ -155,54 +149,38 @@ const getGSCData = async () => {
                   !items.some((actualItem) => actualItem.id === newItem.id)
               );
 
-              console.log("Example one new item");
-              console.log(filteredItems[0]);
-
               if (filteredItems.length === 0) {
                 console.log("No new items to upload");
+                process.exit(0);
               } else {
                 console.log(
-                  `There are ${filteredItems.length} new items to upload para subir`
+                  `There are ${filteredItems.length} new items to upload`
                 );
                 await seoPageSheet.addRows(filteredItems);
                 console.log(
                   `The new ${filteredItems.length} items were uploaded`
                 );
+                process.exit(0);
               }
-              process.exit(0); // Sucesso
             }
           } catch (error) {
             console.error(error.message);
-            process.exit(1); // Erro
+            process.exit(1);
           }
-        }
+        };
 
-        getData();
+        await getData(); // Await the execution of getData
       });
+
+      await Promise.all(sitePromises); // Wait for all getData executions to complete
     } else {
       console.log("No website found.");
-      process.exit(1); // Erro
+      process.exit(1);
     }
-
-    await getData(site.siteUrl);
   } catch (error) {
     console.error("Error by authenticating service account:", error);
-    process.exit(1); // Erro
+    process.exit(1);
   }
-
-  const googleSheetCredentials = {
-    type: process.env.GOOGLE_SHEETS_TYPE,
-    project_id: process.env.GOOGLE_SHEETS_PROJECT_ID,
-    private_key_id: process.env.GOOGLE_SHEETS_PRIVATE_KEY_ID,
-    private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-    client_id: process.env.GOOGLE_SHEETS_CLIENT_ID,
-    auth_uri: process.env.GOOGLE_SHEETS_AUTH_URI,
-    token_uri: process.env.GOOGLE_SHEETS_TOKEN_URI,
-    auth_provider_x509_cert_url:
-      process.env.GOOGLE_SHEETS_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.GOOGLE_SHEETS_CLIENT_CERT_URL,
-  };
 };
 
 module.exports = getGSCData;
